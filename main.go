fd79f3949b35f9f8a615f19940b9a2f601ab85bc
Muhammad Arif Martadinata <130190908+muhammadarifmartadinataa@users.noreply.github.com>
2024-12-06 14:46:54 +0700

    Feat/latest report (#34)
    * add artikel get by id, and fix validate
    
    * feat(artikel-id) : Fixing GET ARTIKEL BY ID
    
    * feat(artikel-id) : Fixing GET ARTIKEL BY ID
    
    * feat(admin-latest-report) : GET 10 Report
    
    ---------
    
    Co-authored-by: reihaniz <reihaniz.pr@gmail.com>

--- main.go
+++ main.go
@@ -73,6 +73,8 @@ func protectedRoutes(e *echo.Echo) {
 	adminGroup.GET("/users", controllers.GetAllUsers)
 	adminGroup.GET("/users/:id", controllers.GetUserByID) // Mendapatkan user berdasarkan ID
 
+	adminGroup.GET("/latest-report", controllers.GetLatestReports)
+
 	// Rute Artikel Edukasi
 	adminGroup.POST("/articles", controllers.BikinArtikel)
 	authGroup.GET("/articles", controllers.AmbilSemuaArtikel)
