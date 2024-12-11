654705b49ca6fcd76ee7b641c5a4a7b7c68f3deb
Muhammad Arif Martadinata <muhammadarifmartadinata@gmail.com>
2024-12-11 13:03:06 +0700

    feat(reporting) : get user by id + report by id

--- models/user.go
+++ models/user.go
@@ -5,15 +5,16 @@ import (
 )
 
 type User struct {
-	ID           uint      `gorm:"primaryKey;autoIncrement" json:"id_user"`
-	NamaLengkap  string    `gorm:"type:varchar(255)" json:"nama_lengkap"`
-	TanggalLahir time.Time `gorm:"type:datetime" json:"tanggal_lahir"`
-	NoTelepon    string    `gorm:"type:varchar(15)" json:"no_telepon" validate:"regexp"`
-	Password     string    `gorm:"type:varchar(255)" json:"password"`
-	Email        string    `gorm:"type:varchar(255);unique;not null" json:"email"`
-	Role         string    `gorm:"type:varchar(50);default:'user'" json:"role"`
-	Photo        string    `gorm:"type:varchar(255)" json:"photo"`
-	Points       uint      `gorm:"default:0" json:"points"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
+	ID           uint            `gorm:"primaryKey;autoIncrement" json:"id_user"`
+	NamaLengkap  string          `gorm:"type:varchar(255)" json:"nama_lengkap"`
+	TanggalLahir time.Time       `gorm:"type:datetime" json:"tanggal_lahir"`
+	NoTelepon    string          `gorm:"type:varchar(15)" json:"no_telepon" validate:"regexp"`
+	Password     string          `gorm:"type:varchar(255)" json:"password"`
+	Email        string          `gorm:"type:varchar(255);unique;not null" json:"email"`
+	Role         string          `gorm:"type:varchar(50);default:'user'" json:"role"`
+	Photo        string          `gorm:"type:varchar(255)" json:"photo"`
+	Points       uint            `gorm:"default:0" json:"points"`
+	Reports      []ReportRubbish `gorm:"foreignKey:UserID" json:"reports"`
+	CreatedAt    time.Time       `json:"created_at"`
+	UpdatedAt    time.Time       `json:"updated_at"`
 }
